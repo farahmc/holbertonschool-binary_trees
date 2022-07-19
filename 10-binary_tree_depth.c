8f44882523b1a99a4fbf425251d2705699d9f8fa
declannoble <declanjnoble@hotmail.com>
2022-07-18 21:38:43 -0700

    docs: add docs

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_depth - mesaures binary tree depth
+ * @tree: pointer to node
+ * Return: if tree is null, return null otherwise return depth
+ */
+
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t count = 0;
