53fa561477f15be2b9f63783609e1869ab2ed39a
Farah <farah.mccurdy@gmail.com>
2022-07-20 18:45:09 -0700

    change: recursive

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -59,7 +59,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (binary_tree_balance(tree) != 0)
 		return (0);
 	
-	if ((binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right))
+	if ((binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right)))
 		return (1);
 	return (0);
 
