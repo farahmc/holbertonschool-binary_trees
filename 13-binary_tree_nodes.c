b5445e7d7f004c565dc458e9bb1ca4a981618d7b
Farah <farah.mccurdy@gmail.com>
2022-07-20 17:08:13 -0700

    fix: betty

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -15,7 +15,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 	if (tree->right == NULL && tree->left == NULL)
 		return (0);
-	
+
 	left = binary_tree_nodes(tree->left);
 	right = binary_tree_nodes(tree->right);
 	return (1 + left + right);
