a822c9d7a6a500403baacca5ea8cab5b7433a4cd
declannoble <97944678+declannoble@users.noreply.github.com>
2022-07-21 11:41:43 +1000

    Update 16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -59,8 +59,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (binary_tree_balance(tree) != 0)
 		return (0);
 	
-	if (tree->left != NULL && tree->right != NULL)
-		return (binary_tree_is_perfect(tree->left) + binary_tree_is_perfect(tree->right));
+	if ((binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right))
+		return (1);
 	return (0);
 
 
