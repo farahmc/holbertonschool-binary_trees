d4417058ddb3911de53971bba54ab60d239ff467
Farah <farah.mccurdy@gmail.com>
2022-07-20 18:51:32 -0700

    fix: betty, change: tidied up early return

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -49,19 +49,15 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	
-	if (tree == NULL)
+	if (tree == NULL || binary_tree_balance(tree) != 0)
 		return (0);
 
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 
-	if (binary_tree_balance(tree) != 0)
-		return (0);
-	
-	if ((binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right)))
+	if ((binary_tree_is_perfect(tree->left) &&
+	     binary_tree_is_perfect(tree->right)))
 		return (1);
+
 	return (0);
-
-
 }
