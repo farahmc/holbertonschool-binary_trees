d45998bd4d216e1476319de69df8a0a60868a4c0
Farah <farah.mccurdy@gmail.com>
2022-07-20 17:48:12 -0700

    fix: height func

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,25 +1,28 @@
 #include "binary_trees.h"
 
 /**
- * height- find the height of a binary tree
- * @tree: the tree to assess
+ * height- measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height
  *
- * Return: the tallest height
+ * Return: height of tree
  */
 
 size_t height(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (-1);
+	if (tree != NULL)
+	{
+		int left_height = height(tree->left);
+		int right_height = height(tree->right);
 
-	int left_height = height(tree->left);
-	int right_height = height(tree->right);
+		if (left_height > right_height)
+			return (left_height + 1);
+		else
+			return (right_height + 1);
+	}
 
-	if (left_height >= right_height)
-		return (left_height + 1);
-	else
-		return (right_height + 1);
+	return (-1);
 }
+
 /**
  * binary_tree_balance- measures the balance factor of a binary tree
  * @tree: pointer to the root node of the tree to measure the balance factor
