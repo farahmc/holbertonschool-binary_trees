0a7723ad59aa84d357aa1ac5bac022ee31dfe95e
declannoble <97944678+declannoble@users.noreply.github.com>
2022-07-21 10:33:47 +1000

    refactor: remove variables

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,14 +8,8 @@
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t left;
-	size_t right;
-
 	if (tree == NULL)
 		return(0);
-
-	left= binary_tree_size(tree->left);
-	right = binary_tree_size(tree->right);
-	return (1 + left + right);
-	// +1 for the root node
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+	
 }
